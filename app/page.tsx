210410704966f1147560396b35cabc56aec6faba
Caio Portella <caiohportella@gmail.com>
2024-07-31 17:26:55 -0300

    vercel url problem

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const url = `${
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : `https://${process.env.VERCEL_URL}`
+      : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   }`;
 
   return (
